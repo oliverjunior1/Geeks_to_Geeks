e87f5bf1fb581082cc5d40658a4058f65482b33b
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-16 16:51:36 -0300

    Update begin.py

--- numpy_trainer/begin.py
+++ numpy_trainer/begin.py
@@ -1,9 +1,15 @@
+# import numpy as np
+#
+# x = np.array([1,2,3])
+#
+# y = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#
+# z = np.array((1,2,3))
+#
+# print(f'Simple array \n{x},\n double array\n {y}\n and array for tuple\n{z}')
+
 import numpy as np
 
-x = np.array([1,2,3])
+x =np.array([1,3,5,8])
 
-y = np.array([[1,2,3],[4,5,6],[7,8,9]])
-
-z = np.array((1,2,3))
-
-print(f'Simple array \n{x},\n double array\n {y}\n and array for tuple\n{z}')
+print(x)
