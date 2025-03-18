2dba28d7178464e8f19b8d11a4885b17c8dcf710
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-18 19:33:17 -0300

    Update class_person.py

--- Python_concepts/class_person.py
+++ Python_concepts/class_person.py
@@ -6,8 +6,6 @@ class Person:
     def __str__(self):
         return f"The name is {self.name} and the age is {self.age} years old."
 
-person = Person("Joao Pedro", 12)
-person1 = Person("Mariane Vitoria", 4)
+person = Person("Mariane Vitoria", 4)
 
 print(person)
-print(person1)
