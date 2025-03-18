17d7f8b56fc674a22c1e0bd28fe56adc459f917c
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-18 20:33:20 -0300

    Update draw_write_type.py

--- Pygame_trainer/draw_write_type.py
+++ Pygame_trainer/draw_write_type.py
@@ -16,9 +16,15 @@ text_rect.center=(250,250)
 
 # Ending
 while True:
-    
+    screen.blit(text, text_rect)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
             pygame.quit()
+        if event.type== pygame.KEYDOWN:
+            if event.key == pygame.K_0:
+                screen.fill('lightblue')
+            if event.key == pygame.K_1:
+                screen.fill('white')
+
     pygame.display.flip()
