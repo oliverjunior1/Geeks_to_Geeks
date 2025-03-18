474387b6deba35e9e0bb37821e3c2572c63d30c5
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-18 20:16:52 -0300

    Update draw_write_type.py

--- Pygame_trainer/draw_write_type.py
+++ Pygame_trainer/draw_write_type.py
@@ -6,24 +6,19 @@ pygame.init()
 screen = pygame.display.set_mode((500,500))
 
 # draw
-draw = pygame.draw.rect(screen, 'red', (50,50,30,30))
+draw = pygame.draw.rect(screen, 'red', (40,40,30,30))
 
-# text
+# write
 font = pygame.font.SysFont('Times New Roman', 25)
-text = font.render('Jesus is the light of the world!', True, 'red')
+text = font.render('Jesus is the light of the world!', True, 'pink')
 text_rect = text.get_rect()
 text_rect.center=(250,250)
 
+# Ending
 while True:
-    screen.blit(text, text_rect)
+    
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
             pygame.quit()
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_0:
-                screen.fill('blue')
-            if event.key==pygame.K_1:
-                screen.fill('gray')
     pygame.display.flip()
-
