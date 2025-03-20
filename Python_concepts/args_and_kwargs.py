4b361aa8261bd8cb2e58c7c7f3dcba559c16c588
Joaquim Rodrigues de Oliveira Junior <144371202+oliverjunior1@users.noreply.github.com>
2025-03-19 21:05:30 -0300

    Update args_and_kwargs.py

--- Python_concepts/args_and_kwargs.py
+++ Python_concepts/args_and_kwargs.py
@@ -1,5 +1,5 @@
-def kwargs_and_args(*args, **kwargs):
-    print(f"The args are {args}\n The kwargs are {kwargs}")
+def args_kwargs(*args, **kwargs):
+    print(f'args:{args}, kwars:{kwargs}')
 
-print(kwargs_and_args(1,2,3, a=4, b=5, c=6))
+args_kwargs(1,2,3,4,a=5, b=6)
 
