ae2859c56d316f70c37d0617498532c5ed96290e
oliverjunior1 <oliver-junior@hotmail.com>
2025-05-08 19:50:50 -0300

    Update class_person.py

--- Python_concepts/class_person.py
+++ Python_concepts/class_person.py
@@ -1,13 +1,19 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# Big bang, two and a half man and family
+
+class Big_bang:
+    def __init__(self, character, comic_books):
+        self.character = character
+        self.comic_books = comic_books
 
     def __str__(self):
-        return f"The name is {self.name} and the age is {self.age} years old."
+        return f"The character {self.character} has {self.comic_books} comic-books in his bedroom."
 
-person = Person("Joao Pedro", 12)
-person1 = Person("Mariane Vitoria", 4)
+big_bang = Big_bang("Sheldon", 25000)
+big_bang1 = Big_bang("Leonard", 21000)
+big_bang2 = Big_bang("Howard", 15060)
+big_bang3 = Big_bang("Hajesh", 8008)
 
-print(person)
-print(person1)
+print(big_bang)
+print(big_bang1)
+print(big_bang2)
+print(big_bang3)
