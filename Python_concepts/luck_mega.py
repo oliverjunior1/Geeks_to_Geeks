8295b16040ff82af5a9408ccd9d0ca6d6af966e4
oliverjunior1 <oliver-junior@hotmail.com>
2025-05-11 16:35:46 -0300

    Update luck_mega.py

--- Python_concepts/luck_mega.py
+++ Python_concepts/luck_mega.py
@@ -1,7 +1,7 @@
 import random
 
-def mega():
-    x = sorted(random.sample(range(1,61), 6))
-    return x
+def lotofacil():
+    x = sorted(random.sample(range(1,26),15))
+    return print(x)
 
-print(mega())
+lotofacil()
