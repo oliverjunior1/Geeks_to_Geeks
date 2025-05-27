2731bd7a3ebe798d75a53e426515559fab9863cf
oliverjunior1 <oliver-junior@hotmail.com>
2025-05-27 19:49:11 -0300

    Update matplot_with_pie.py

--- matplotlib_trainer/matplot_with_pie.py
+++ matplotlib_trainer/matplot_with_pie.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-code = ['Python', 'Javascript', 'Java', 'C#', "Nocode", "Others"]
+code = ['1_Python', 'Javascript', 'Java', 'C#', "Nocode", "Others"]
 percent = [25,20,16,10,9,19]
 
 plt.pie(percent, labels=code)
