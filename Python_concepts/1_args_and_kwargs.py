267d48025103863955068add3ec24fbb3cea107f
oliverjunior1 <oliver-junior@hotmail.com>
2025-06-03 18:42:23 -0300

    Update 1_args_and_kwargs.py

--- Python_concepts/1_args_and_kwargs.py
+++ Python_concepts/1_args_and_kwargs.py
@@ -1,7 +1,8 @@
-# args and kwargs with five elements
+# args and kwargs with four elements
 
 def args_and_kwargs(*args, **kwargs):
     print(args, kwargs)
 
-args_and_kwargs(1,2,3,4,5, num=1, num1=2, num2=3, num3=4, num4=5)
+args_and_kwargs(1,2,3,4, a=1, b=2, c=3, d=4)
+
 
