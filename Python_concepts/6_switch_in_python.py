51bce37b9a298ad949c231fa6e34f87f3531178d
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-04 19:06:39 -0300

    Update 6_switch_in_python.py

--- Python_concepts/6_switch_in_python.py
+++ Python_concepts/6_switch_in_python.py
@@ -1,10 +1,23 @@
-def switch_case(option):
-    switch={
-        1:"Option 1",
-        2:"Option 2",
-        3:"Option 3"
-    }
-    return switch.get(option, "invalid")
+day = int(input("Type a number to see the day: "))
 
-user_option = int(input("Choice a number to 1 to 3:"))
-print(switch_case(user_option))
+while day !=10:
+    match day:
+        case 1:
+            print("Sunday")
+        case 2:
+            print("Monday")
+        case 3:
+            print("Tuesday")
+        case 4:
+            print("Wednesday")
+        case 5:
+            print("Thursday")
+        case 6:
+            print("Friday")
+        case 7:
+            print("Saturday")
+        case 10:
+            break
+        case _:
+            print("Not a valid number")
+    day = int(input("Type a number to see the day: "))
