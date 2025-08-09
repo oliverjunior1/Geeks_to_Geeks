435e7b06670b9a2c93696048c182a27473de258c
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-09 15:12:53 -0300

    Update 1_args_and_kwargs.py

--- Python_concepts/1_args_and_kwargs.py
+++ Python_concepts/1_args_and_kwargs.py
@@ -5,12 +5,3 @@ def args_kwargs(*args, **kwargs):
 
 args_kwargs(1,2,3,a=1,b=2, c=3)
 
-#Do args with arithmetics operations
-
-def args_with_arithmetics(*args):
-    sum = 0
-    for x in args:
-        sum += x
-    return print(sum)
-
-args_with_arithmetics(10,20,30,40,50)
