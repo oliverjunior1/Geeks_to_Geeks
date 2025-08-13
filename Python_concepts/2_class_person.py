4e39ae45b23a05c32c92e3ce625e2ac98935ca60
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-13 19:42:44 -0300

    Update 2_class_person.py

--- Python_concepts/2_class_person.py
+++ Python_concepts/2_class_person.py
@@ -1,15 +1,13 @@
-# Two and a half man
+# Big Bang Theory
 
-class Two_and_a_half_man:
-    def __init__(self, character, dolls):
-        self.character = character
-        self.dolls = dolls
+class Big_bang:
+    def __init__(self, name, comic_books):
+        self.name = name
+        self.comic_books = comic_books
 
     def __str__(self):
-        return f"The character {self.character} has {self.dolls} dolls in his bedroom."
+        return f"The charracter {self.name} has {self.comic_books} in his bedroom."
 
-charlie = Two_and_a_half_man("Charlie", 2)
-allan = Two_and_a_half_man("Allan", 100)
+sheldon = Big_bang("Sheldon", 25000)
 
-print(charlie)
-print(allan)
+print(sheldon)
