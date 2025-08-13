22b329eaea3a285703c66e1731d2b77c68bb30e0
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-13 19:39:40 -0300

    Update 1_args_and_kwargs_2.py

--- Python_concepts/1_args_and_kwargs_2.py
+++ Python_concepts/1_args_and_kwargs_2.py
@@ -2,9 +2,8 @@
 
 def args_and_kwargs(*args, **kwargs):
     sum = 0
-    sum2 = 0
     for x in args:
         sum += x
     return sum
 
-print(args_and_kwargs(50,50,50,50, a=20, b=50))
+print(args_and_kwargs(10,20,3,4,50))
