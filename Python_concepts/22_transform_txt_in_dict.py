f286b7774ad211a9f23a73ad334f95d0da952bfc
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-17 17:07:58 -0300

    Update 22_transform_txt_in_dict.py

--- Python_concepts/22_transform_txt_in_dict.py
+++ Python_concepts/22_transform_txt_in_dict.py
@@ -3,7 +3,7 @@ dicionario = {}
 with open('dados.txt', 'r') as arquivo:
     for linha in arquivo:
         if ':' in linha:
-            chave, valor = linha.strip().split(':',1)
+            chave, valor = linha.strip().split(':', 1)
             dicionario[chave.strip()] = valor.strip()
 
 print(dicionario)
