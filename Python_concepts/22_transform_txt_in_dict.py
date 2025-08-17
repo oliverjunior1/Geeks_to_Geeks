c851945f7bdd6de2fd59e2a9147f571a7b951563
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-17 20:03:44 -0300

    Update 22_transform_txt_in_dict.py

--- Python_concepts/22_transform_txt_in_dict.py
+++
@@ -1,9 +0,0 @@
-dicionario = {}
-
-with open("dados.txt", 'r') as arquivo:
-    for line in arquivo:
-        if ':' in line:
-            chave, valor = line.strip().split(':',1)
-            dicionario[chave.strip()] = valor.strip()
-
-print(dicionario)
