40a8c50355102a4bcba17d2a96f00a63816dbcd0
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-17 16:43:12 -0300

    Update 23_timer.py

--- Python_concepts/23_timer.py
+++ Python_concepts/23_timer.py
@@ -2,11 +2,4 @@
 # time.sleep(5)
 # print("Estamos voltando")
 
-sum = 0
-while True:
-  num = int(input("coloque os números para ver a soma ou 00 para sair: "))
-  if num != 00:
-    sum += num
-  else:
-    print(sum)
-    break
+dicionario =
