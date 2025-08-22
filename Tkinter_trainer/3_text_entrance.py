61e143ef19a9b3b1ef3a0c5f816c52254d96f0d1
oliverjunior1 <oliver-junior@hotmail.com>
2025-08-21 21:21:40 -0300

    Create 3_text_entrance.py

---
+++ Tkinter_trainer/3_text_entrance.py
@@ -0,0 +1,14 @@
+# from customtkinter import CTk, CTkLabel, CTkEntry
+#
+#
+# def mostrar_nome():
+#     nome = entrada.ge()
+#     rotulo.config(text=f"Olá´, {nome}")
+#
+# window = CTk()
+# window.title("Entrada Nome")
+#
+# entrada = CTkEntry(window)
+# entrada.pack()
+#
+# botao =
