27cbc7d4aab7ea59fa098596a4a6447044619ab1
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-02 18:40:20 -0300

    Update 22_transform_txt_in_dict.py

---
+++ Python_concepts/22_transform_txt_in_dict.py
@@ -0,0 +1,7 @@
+dicionario = {}
+
+with open('dados.txt', 'r') as document:
+    for linha in document:
+            if ':' in linha:
+                chave, valor = linha.strip().split(':', 1)
+                
