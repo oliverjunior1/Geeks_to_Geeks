467f04ae0d2437026a4cf90214805170ce44e9d3
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-12 19:10:20 -0300

    Create 3_dicionario_de_alunos_e_notas.py

---
+++ Python_concepts/Treinamento_adicional_dict/3_dicionario_de_alunos_e_notas.py
@@ -0,0 +1,11 @@
+alunos = {
+    "Ana":8.5,
+    "Bruno":7.0,
+    "Carlos":9.2,
+    "Diana":6.8
+}
+
+soma = sum(alunos.values())
+media = soma/len(alunos)
+
+print(f"Média geral da turma: {media:.2f}")
