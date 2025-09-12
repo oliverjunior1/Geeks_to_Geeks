0c35ababbb6454fadb2067a1200fe4443e6c4110
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-12 20:00:31 -0300

    Update 3_dicionario_de_alunos_e_notas.py

--- Python_concepts/Treinamento_adicional_dict/3_dicionario_de_alunos_e_notas.py
+++ Python_concepts/Treinamento_adicional_dict/3_dicionario_de_alunos_e_notas.py
@@ -1,11 +1,13 @@
+# Criar um dicionário com nomes de alunos e suas notas. Calcular a média geral.
+
 alunos = {
     "Ana":8.5,
     "Bruno":7.0,
-    "Carlos":9.2,
-    "Diana":6.8
+    "Joaquim":6.7,
+    "Joao":7.5
 }
 
 soma = sum(alunos.values())
 media = soma/len(alunos)
 
-print(f"Média geral da turma: {media:.2f}")
+print(media)
