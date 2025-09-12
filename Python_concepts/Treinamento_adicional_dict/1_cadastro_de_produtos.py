3527c7325260316ad3165a08a98fd88823656b4f
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-12 20:11:08 -0300

    Update 1_cadastro_de_produtos.py

--- Python_concepts/Treinamento_adicional_dict/1_cadastro_de_produtos.py
+++ Python_concepts/Treinamento_adicional_dict/1_cadastro_de_produtos.py
@@ -1,16 +1,14 @@
 # Objetivo: Criar um dicionário com produtos e seus preços. Permitir ao usuário consultar o preço
 # de um produto digitando o nome.
 
-produtos = {
+precos = {
     "teclado":14.99,
-    "mouse":14.50,
-    "notebook":2599.99,
-    "impressora":399.99
+    "mouse":12.00,
+    "notebook":2999.00,
+    "gabinete":200.00
 }
 
-produto_search = input("Digite o produto para ver o preço? ")
+procurar_produto = input("Clique para procurar o produto: ")
 
-if produto_search in produtos:
-    print(produtos[produto_search])
-else:
-    print("Produto inválido")
+if procurar_produto in precos:
+    print(precos[procurar_produto])
