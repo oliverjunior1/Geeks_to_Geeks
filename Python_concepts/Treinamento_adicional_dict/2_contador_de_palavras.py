67fdea5bd7c1301b34c403f5394481681bfad8b1
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-12 20:37:56 -0300

    Update 2_contador_de_palavras.py

--- Python_concepts/Treinamento_adicional_dict/2_contador_de_palavras.py
+++ Python_concepts/Treinamento_adicional_dict/2_contador_de_palavras.py
@@ -8,11 +8,26 @@ a = {
     'w':4
 }
 
+# Acessar valor pela chave
 print(a['x'])
+
+# Adicionar novo par
 a['b']=5
 print(a)
+
+# Alterar valor
+a['y'] = 5
+print(a)
+
+# Remover chave
 del a['b']
 print(a)
+
+# Ver todas as chaves
 print(a.keys())
+
+# Ver todos os valores
 print(a.values())
+
+# Ver todos os pares
 print(a.items())
