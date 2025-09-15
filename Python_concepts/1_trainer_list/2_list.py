d033365a9052970e4434737b53cb3365434214d1
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-15 17:28:07 -0300

    Update 2_list.py

--- Python_concepts/1_trainer_list/2_list.py
+++ Python_concepts/1_trainer_list/2_list.py
@@ -3,9 +3,16 @@ thisdict = {
     'model':'Mustang',
     'year':1964
 }
-
+# Exclude the model
+#
 # thisdict.pop('model')
 # print(thisdict)
+#
+# The diference between is when you put without a definition, the pop withdraw for the final
+# and the del don't accept
+#
+# del thisdict['model']
+# print(thisdict)
 
-del thisdict['model']
-print(thisdict)
+# thisdict.clear()
+# print(thisdict)
