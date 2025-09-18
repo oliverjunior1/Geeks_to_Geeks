44ce3ae627a2542052187111fc3a1ac4799febb9
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-18 20:09:56 -0300

    Update 3_lambda_pow.py

--- Python_concepts/3_lambda_pow.py
+++ Python_concepts/3_lambda_pow.py
@@ -1,12 +1,11 @@
+import math
 while True:
-    a = int(input("Type a number: "))
-    b = int(input("Type another number: "))
-    c = int(input("Type another different number or 00 to exit: "))
-    if c==00:
-        break
-    else:
-        x = lambda a,b,c: (a**b)//c
+    a = float(input("Coloque um numero: "))
+    b = float(input("Coloque outro numero: "))
+    c = float(input("Coloque um outro numero: "))
 
-        print(x(a,b,c))
+    x = lambda a, b, c: math.pow((a, b),c)
+    print(x)
+
 
 
