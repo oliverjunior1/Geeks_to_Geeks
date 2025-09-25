7f9e1f20185e82f3b86962d9a8607be19fc636ef
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-24 23:45:52 -0300

    Update 1_list_tuples_sets_dict_array.py

--- Python_concepts/1_list_tuples_sets_dict_array.py
+++ Python_concepts/1_list_tuples_sets_dict_array.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-a = [1,2,3]
+a = [1,2,3,4]
 print(type(a))
 
-b = (1,2,3)
+b = (1,2,3,4)
 print(type(b))
 
 c = {'a':1, 'b':2, 'c':3}
 print(type(c))
 
-d = {1,2,3}
+d = {1,2,3,4}
 print(type(d))
 
-e = pd.array([1,2,3])
+e = pd.array([1,2,3,4])
 print(type(e))
