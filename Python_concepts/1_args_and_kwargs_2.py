8cabf5dabd9873346aba79f61fe8aec7bfb1798e
oliverjunior1 <oliver-junior@hotmail.com>
2025-09-24 23:41:12 -0300

    Update 1_args_and_kwargs_2.py

--- Python_concepts/1_args_and_kwargs_2.py
+++ Python_concepts/1_args_and_kwargs_2.py
@@ -1,9 +1,9 @@
 #Do args with arithmetics operations
 
 def args_sum(*args):
-    sum = 0
-    for x in args:
-        sum += x
+    sum =0
+    for arg in args:
+        sum -= arg
     print(sum)
 
-args_sum(15,15,15,15,15,15)
+args_sum(1,2,3,4,5)
