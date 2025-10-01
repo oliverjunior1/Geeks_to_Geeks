b2c8ea4633ce516be242d90f4b9e794540519584
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-01 17:20:01 -0300

    Update 1_args_and_kwargs_2.py

--- Python_concepts/1_args_and_kwargs_2.py
+++ Python_concepts/1_args_and_kwargs_2.py
@@ -3,7 +3,9 @@
 def args_sum(*args):
     sum =0
     for arg in args:
-        sum -= arg
-    print(sum)
+        sum += arg
+    return sum
 
-args_sum(1,2,3,4,5)
+x =args_sum(10,10,10,10)
+
+print(x)
