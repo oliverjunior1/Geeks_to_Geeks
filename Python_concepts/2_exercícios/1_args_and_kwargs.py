9ea3877706cf1088aa16e33a3bc96e5ab8bdf29e
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-07 20:37:17 -0300

    Update 1_args_and_kwargs.py

--- Python_concepts/2_exercícios/1_args_and_kwargs.py
+++ Python_concepts/2_exercícios/1_args_and_kwargs.py
@@ -1,19 +1,4 @@
-'''Exercício: Função de Cadastro de Usuário
-Crie uma função chamada cadastrar_usuario que aceita:
-• 	Um número indefinido de argumentos posicionais (*args) representando hobbies do usuário.
-• 	Um número indefinido de argumentos nomeados (**kwargs) representando informações pessoais como nome, idade, cidade, etc.
-A função deve:
-1. 	Imprimir as informações pessoais de forma organizada.
-2. 	Imprimir a lista de hobbies, um por linha.'''
+def args_and_kwargs(*args, **kwargs):
+    print(args, kwargs)
 
-# criar a funcao cadastro de usuário
-def cadastrar_usuario(*args, **kwargs):
-    contadora = 0
-    contadorb = 0
-    for x in args:
-        print(f"Hobbies do usuário: {x}")
-    for a in kwargs:
-        print(f"{a}:{kwargs[a]}")
-
-
-cadastrar_usuario('futebol', 'video game', nome='Joaquim', altura=1.74)
+args_and_kwargs(1,2,3,a=1, b=2, c=3)
