2041d597b33d9d54ec193bc082fdac377171b1d0
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-17 23:12:36 -0300

    Update 1_basic_custom_tkinter.py

--- Custom_Tkinter_trainer/1_basic_custom_tkinter.py
+++ Custom_Tkinter_trainer/1_basic_custom_tkinter.py
@@ -1,10 +1,1 @@
-import customtkinter as ctk
 
-window = ctk.CTk()
-window.title("Minha primeira janela")
-window.geometry("380x200")
-
-rotulo = ctk.CTkLabel(window, text="Olá, Joaquim")
-rotulo.pack()
-
-window.mainloop()
