e8cc72fc884fc6f95efde31298a925140558065c
oliverjunior1 <oliver-junior@hotmail.com>
2025-10-17 23:32:30 -0300

    Update 1_basic_custom_tkinter.py

--- Custom_Tkinter_trainer/1_basic_custom_tkinter.py
+++ Custom_Tkinter_trainer/1_basic_custom_tkinter.py
@@ -1,1 +1,10 @@
+import customtkinter as ctk
 
+window = ctk.CTk()
+window.title("Minha primeira janela")
+window.geometry("380x200")
+
+rotulo = ctk.CTkLabel(window, text="Jesus is the light")
+rotulo.pack()
+
+window.mainloop()
